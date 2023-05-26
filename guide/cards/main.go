12a9b3a591612a8a9d3e4ad4f6f24ed3f76586d7
jrm2087 <jrm2087@gmail.com>
2023-05-25 20:13:53 -0500

    variables - funciones

--- guide/cards/main.go
+++ guide/cards/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//var card string = "Ace of Spades"
+	//card := "Ace of Spades"
+	//card = "Five of Diamonds"
+	card := newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
