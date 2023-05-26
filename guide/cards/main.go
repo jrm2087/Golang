ff43e552dc616185ea4d8d1bb132bd17cefb6131
jrm2087 <jrm2087@gmail.com>
2023-05-25 20:27:20 -0500

    slices - for loop

--- guide/cards/main.go
+++ guide/cards/main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+//func main() {
+//	//var card string = "Ace of Spades"
+//	//card := "Ace of Spades"
+//	//card = "Five of Diamonds"
+//	card := newCard()
+//	fmt.Println(card)
+//}
+
 func main() {
-	//var card string = "Ace of Spades"
-	//card := "Ace of Spades"
-	//card = "Five of Diamonds"
-	card := newCard()
-	fmt.Println(card)
+	// slice
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
