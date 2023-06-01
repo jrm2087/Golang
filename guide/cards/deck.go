3e7bbf3b04e8ce71b698cf7fc92934c8c710c42c
jrm2087 <jrm2087@gmail.com>
2023-05-31 20:45:00 -0500

    funciones y paquetes

--- guide/cards/deck.go
+++ guide/cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -26,4 +30,14 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	//return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+
 }
--- guide/cards/main.go
+++ guide/cards/main.go
@@ -10,8 +10,13 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
+	//hand, remainingCards := deal(cards, 5)
+	//
+	//hand.print()
+	//remainingCards.print()
 
-	hand.print()
-	remainingCards.print()
+	//fmt.Println(cards.toString())
+
+	cards.saveToFile("my_cards2")
+
 }
