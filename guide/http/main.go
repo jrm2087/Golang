c88b06cfbe95d882b50a26d6e54c80ec3de05e2f
jrm2087 <jrm2087@gmail.com>
2023-08-29 18:58:07 -0500

    interfaz implementacion

--- guide/http/main.go
+++ guide/http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com/")
@@ -14,6 +17,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	//fmt.Println(resp)
+	//bs := make([]byte, 999999)
+	//resp.Body.Read(bs)
+	//fmt.Println(string(bs))
 
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("bytes -> ", len(bs))
+	return len(bs), nil
 }
