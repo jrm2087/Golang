a74c6774821a3dc555277d443a6306e34161e5a8
jrm2087 <jrm2087@gmail.com>
2023-09-01 21:01:33 -0500

    channels implementation

--- guide/channels/main.go
+++ guide/channels/main.go
@@ -14,17 +14,23 @@ func main() {
 		//"http://sasasasasas.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down.")
+		c <- "channel message"
 		return
 	}
 	fmt.Println(link, "is up.")
+	c <- "It's up"
 }
